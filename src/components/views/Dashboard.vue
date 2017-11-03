cbdfdd91e9e53d14dc2606cc205135c9668792a6
William Bellone <william.bellone@gmail.com>
2017-11-03 15:18:56 +0100

    update dashboard

--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="alert alert-success alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <h4><i class="icon fa fa-check"></i> SAmouuuuuuuuuu</h4>
+        <h4><i class="icon fa fa-check"></i> edit SAmouuuuuuuuuu</h4>
         Click on icon to check it out on github. <a href="https://github.com/misterGF/CoPilot" target="_blank"><i class="fa fa-github fa-2x"></i></a>
       </div>
       <div class="col-md-3 col-sm-6 col-xs-12">
